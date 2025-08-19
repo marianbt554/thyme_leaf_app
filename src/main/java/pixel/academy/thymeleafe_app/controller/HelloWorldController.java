d6bc9e1a35ce7dad6061ca471b3c1d47b2453df8
marianbt554 <botnarencomarian8@gmail.com>
2025-08-19 18:50:31 +0200

    Add controller

---
+++ src/main/java/pixel/academy/thymeleafe_app/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package pixel.academy.thymeleafe_app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //metoda pentru a afisa formularul HTML initial
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+}
