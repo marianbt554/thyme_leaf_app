073990d5b9c40ea13e92b2dd7dacbe954ead3ca5
marianbt554 <botnarencomarian8@gmail.com>
2025-08-19 17:35:48 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/thymeleafe_app/ThymeleafeAppApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.thymeleafe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/thymeleafe_app/ThymeleafeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.thymeleafe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
