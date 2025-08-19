87e43af7099e8fdfc996cd82fa13e7df7a857129
marianbt554 <botnarencomarian8@gmail.com>
2025-08-19 18:14:04 +0200

    Add html file

---
+++ src/main/java/pixel/academy/thymeleafe_app/controller/DemoController.java
@@ -0,0 +1,17 @@
+package pixel.academy.thymeleafe_app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
